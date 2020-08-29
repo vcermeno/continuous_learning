83c00ecaa7a05cf558d6a5b2691aad3bcd9e386e
vcermeno <vcermeno6@gmail.com>
2020-08-28 18:21:12 -0700

    feat: Connect Create Auction Lambda with DynamoDB

--- serverless/auction-service/src/handlers/createAuction.js
+++ serverless/auction-service/src/handlers/createAuction.js
@@ -1,11 +1,23 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient;
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
 
   const auction = {
+    id: uuid(),
     title,
     status: 'open',
     createdAt: new Date().toISOString,
-  }
+  };
+
+  await dynamoDB.put({
+    TableName: 'AuctionsTable',
+    Item: auction,
+  }).promise();
+
   return {
     statusCode: 201,
     body: JSON.stringify(auction),
