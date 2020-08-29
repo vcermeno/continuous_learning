f6c61f2678f008fbe478d4dcd496fdaca85105ea
vcermeno <vcermeno6@gmail.com>
2020-08-28 19:20:40 -0700

    refactor: add error handling and body parser

--- serverless/auction-service/src/handlers/createAuction.js
+++ serverless/auction-service/src/handlers/createAuction.js
@@ -1,10 +1,15 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
 
 const dynamoDB = new AWS.DynamoDB.DocumentClient;
 
 async function createAuction(event, context) {
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body;
 
   const auction = {
     id: uuid(),
@@ -12,11 +17,15 @@ async function createAuction(event, context) {
     status: 'open',
     createdAt: new Date().toISOString,
   };
-
-  await dynamoDB.put({
-    TableName: process.env.AUCTIONS_TABLE_NAME,
-    Item: auction,
-  }).promise();
+  try{
+    await dynamoDB.put({
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      Item: auction,
+    }).promise();
+  } catch(error){
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 201,
@@ -24,6 +33,9 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
 
 
